d8dd7606febba31d75ce658c51232db608642a17
Dominic Meiser <dmeiser@txcorp.com>
2014-06-09 16:51:18 -0400

    Adding base class for matrix optimization data.

---
+++ include/TxMatrixOptimizationDataBase.hpp
@@ -0,0 +1,31 @@
+#ifndef TX_MATRIX_OPTIMIZATION_DATA_BASE_HPP
+#define TX_MATRIX_OPTIMIZATION_DATA_BASE_HPP
+
+/**
+ * @brief Interface for Tech-X matrix optimization data.
+ *
+ * @sa SparseMatrix, Vector, TxVectorOptimizationData,
+ * TxMatrixOptimizationDataCU, TxMatrixOptimizationDataOCL
+ * */
+class TxMatrixOptimizationDataBase {
+ public:
+  virtual int ingestLocalMatrix(SparseMatrix& A) = 0;
+  virtual int ComputeSPMV(const SparseMatrix& A, Vector& x, Vector& y,
+                          bool copyIn = true, bool copyOut = true) = 0;
+  virtual int ComputeSYMGS(const SparseMatrix& A, const Vector& x, Vector& y,
+                           int numberOfSmootherSteps = 1, bool copyIn = true,
+                           bool copyOut = true) = 0;
+  virtual int ComputeProlongation(const SparseMatrix& Af, Vector& xf,
+                                  bool copyIn, bool copyOut) = 0;
+  virtual int ComputeRestriction(const SparseMatrix& Af, const Vector& rf,
+                                 bool copyIn, bool copyOut) = 0;
+  virtual ~TxMatrixOptimizationDataBase() {}
+
+ private:
+  // Disallow copying to prevent slicing
+  TxMatrixOptimizationDataBase(const TxMatrixOptimizationDataBase&);
+  TxMatrixOptimizationDataBase& operator=(const TxMatrixOptimizationDataBase&);
+};
+
+#endif
+
---
+++ tests/test_TxMatrixOptimizationDataBase.cpp
@@ -0,0 +1,10 @@
+#include <config.h>
+#define BOOST_TEST_MODULE TX_MATRIX_OPTIMIZATION_DATA_BASE
+#include <boost/test/unit_test.hpp>
+
+#include <TxMatrixOptimizationDataBase.hpp>
+
+
+BOOST_AUTO_TEST_CASE(Include) {
+  BOOST_CHECK(true);
+}

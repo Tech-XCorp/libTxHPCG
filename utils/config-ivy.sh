44c022097f9c432612242dbbbe90d5920fb23223
Dominic Meiser <dmeiser@txcorp.com>
2014-06-16 14:30:15 -0600

    Fix install prefix on ivy.

--- utils/config-ivy.sh
+++ utils/config-ivy.sh
@@ -2,8 +2,10 @@
 
 PATH="/src_ivy/dmeiser/PTSOLVE/bin:/scr_ivy/dmeiser/PTSOLVE/openmpi/bin:$PATH" \
     /scr_ivy/dmeiser/PTSOLVE/cmake/bin/cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH=/scr_ivy/dmeiser/PTSOLVE/libTxHPCG \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DENABLE_PARALLEL:BOOL=TRUE \
+    -DENABLE_GPU_AWARE_MPI:BOOL=FALSE \
     -DSUPRA_SEARCH_PATH:PATH=/scr_ivy/dmeiser/PTSOLVE \
     -DCMAKE_C_COMPILER:FILEPATH=/scr_ivy/dmeiser/PTSOLVE/openmpi/bin/mpicc \
     -DCMAKE_CXX_COMPILER:FILEPATH=/scr_ivy/dmeiser/PTSOLVE/openmpi/bin/mpicxx \
